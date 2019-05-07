0149091bc77e56d1ef7578ce0ad682ced0775893
Jacob <wnsl5684@gmail.com>
2019-05-07 20:49:32 +0900

    Updates

---
+++ precache-manifest.18143752393112ea19f749966168cda7.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = [
+  {
+    "revision": "28ccf9781d6075d54678",
+    "url": "/jjm/static/css/main.22c99816.chunk.css"
+  },
+  {
+    "revision": "28ccf9781d6075d54678",
+    "url": "/jjm/static/js/main.28ccf978.chunk.js"
+  },
+  {
+    "revision": "a285ae6d3528a07f058c",
+    "url": "/jjm/static/js/runtime~main.a285ae6d.js"
+  },
+  {
+    "revision": "da1ab209e550dae51516",
+    "url": "/jjm/static/css/2.b276c978.chunk.css"
+  },
+  {
+    "revision": "da1ab209e550dae51516",
+    "url": "/jjm/static/js/2.da1ab209.chunk.js"
+  },
+  {
+    "revision": "310509c95512893dc661bd3a6b0d2a5d",
+    "url": "/jjm/static/media/sidebar-2.310509c9.jpg"
+  },
+  {
+    "revision": "8e9f0a3c5578a20733d5bad0e51c91fb",
+    "url": "/jjm/static/media/sidebar-1.8e9f0a3c.jpg"
+  },
+  {
+    "revision": "2503169017014036cf0dd0dd7c2a2b8a",
+    "url": "/jjm/static/media/sidebar-3.25031690.jpg"
+  },
+  {
+    "revision": "fc9cb0538eb5a4dfd6fbb1bf6dd6189b",
+    "url": "/jjm/static/media/sidebar-4.fc9cb053.jpg"
+  },
+  {
+    "revision": "8880a65c57d7f031579335be153f64a0",
+    "url": "/jjm/static/media/marc.8880a65c.jpg"
+  },
+  {
+    "revision": "d75f73cce5abbfbee4e9ff1f4ca62eb2",
+    "url": "/jjm/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/jjm/precache-manifest.8aaf1fc642b421cb73aa46b78f9695a7.js"
+  "/jjm/precache-manifest.18143752393112ea19f749966168cda7.js"
 );
 
 workbox.clientsClaim();
